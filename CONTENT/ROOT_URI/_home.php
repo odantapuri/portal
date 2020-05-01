2fa0c287a849f3970f539d44b6d10d0f6df52d82
Ayan Dalal <ayan.dalal@olacabs.com>
2020-05-01 19:33:42 +0530

    added banner

--- CONTENT/ROOT_URI/_home.php
+++ CONTENT/ROOT_URI/_home.php
@@ -7,7 +7,7 @@
     <p class="content-desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
   </div>
   <div class="banner-video col-md-4" style="float:right;">
-    <iframe width="380" height="210" src="https://www.youtube.com/embed/tgbNymZ7vqY">
+    <iframe width="380" height="210" src="https://www.youtube.com/embed/tgbNymZ7vqY" style="border: none;">
     </iframe>
   </div>
 </div>
