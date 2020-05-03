3bbb294cd1bd009ea99eca92f56285e2e1225c75
Ayan Dalal <ayan.dalal@olacabs.com>
2020-05-03 17:07:05 +0530

    changes

--- CONTENT/ROOT_URI/pricing.php
+++ CONTENT/ROOT_URI/pricing.php
@@ -59,17 +59,79 @@
 <section id="pricing-quotes" class="pricing-quotes">
   <div class="container text-center" data-aos="fade-up">
     <h2>Pricing Plans</h2>
-    <!-- <div class="tab">
-      <button class="tablinks active" style="font-size:20px;" id="monthlyTab">Monthly</button>
-      <button class="tablinks" style="font-size:20px;" id="yearlyTab">Yearly</button>
-      <button class="tablinks" style="font-size:20px;" id="quarterlyTab">Customized</button>
-    </div> -->
-    <div class="row tab-wrapper active" id="monthlyPlan">
+    <div class="tab">
+      <button class="tablinks active" style="font-size:20px;" id="icseBoard">ICSE</button>
+      <button class="tablinks" style="font-size:20px;" id="cbseBoard">CBSE</button>
+      <button class="tablinks" style="font-size:20px;" id="wbBoard">WB Board</button>
+    </div>
+    <div class="row tab-wrapper active" id="icsePlan">
 
       <div class="col-lg-4 col-md-6">
         <div class="box">
           <h3 class="box-heading">Class 9 and Class 10</h3>
-          <p class="box-desc">Physics / Chemistry / Mathematics / English</p>
+          <p class="box-desc">Physics / Chemistry / Mathematics / Biology / English</p>
+          <h4 class="box-pricing"><sup>₹</sup>800<span> / month / Subject</span></h4>
+          <p class="box-desc">Additional Tool Cost of ₹ 150 + GST</p>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 mt-4 mt-md-0">
+        <div class="box">
+          <h3 class="box-heading">Class 11 and Class 12</h3>
+          <p class="box-desc">Physics / Chemistry / Mathematics / Biology / English</p>
+          <h4 class="box-pricing"><sup>₹</sup>1200<span> / month / Subject </span></h4>
+          <p class="box-desc">Additional Tool Cost of ₹ 150 + GST</p>
+
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 mt-4 mt-lg-0">
+        <div class="box">
+          <h3 class="box-heading">Entrance Examinations</h3>
+          <!--<h4 class="box-pricing"><sup>₹</sup>400<span> / month</span></h4> -->
+          <p class="box-desc">Kishore Vaigyanik Protsahan Yojana (KVPY) and Indian Institutes of Science Education and Research (IISER) <br /></p>
+          <p class="box-desc">Please send an e-mail to <a href="mailto:hr@odantapuri.net.in">hr@odantapuri.net.in</a> regarding your interest and we will get back to you.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="row tab-wrapper" id="cbsePlan">
+
+      <div class="col-lg-4 col-md-6">
+        <div class="box">
+          <h3 class="box-heading">Class 9 and Class 10</h3>
+          <p class="box-desc">Science / Mathematics / English</p>
+          <h4 class="box-pricing"><sup>₹</sup>800<span> / month / Subject</span></h4>
+          <p class="box-desc">Additional Tool Cost of ₹ 150 + GST</p>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 mt-4 mt-md-0">
+        <div class="box">
+          <h3 class="box-heading">Class 11 and Class 12</h3>
+          <p class="box-desc">Physics / Chemistry / Mathematics / Biology / English</p>
+          <h4 class="box-pricing"><sup>₹</sup>1200<span> / month / Subject </span></h4>
+          <p class="box-desc">Additional Tool Cost of ₹ 150 + GST</p>
+
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 mt-4 mt-lg-0">
+        <div class="box">
+          <h3 class="box-heading">Entrance Examinations</h3>
+          <!--<h4 class="box-pricing"><sup>₹</sup>400<span> / month</span></h4> -->
+          <p class="box-desc">Kishore Vaigyanik Protsahan Yojana (KVPY) and Indian Institutes of Science Education and Research (IISER) <br /></p>
+          <p class="box-desc">Please send an e-mail to hr@odantapuri.net.in regarding your interest and we will get back to you.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="row tab-wrapper" id="wbbPlan">
+
+      <div class="col-lg-4 col-md-6">
+        <div class="box">
+          <h3 class="box-heading">Class 9 and Class 10</h3>
+          <p class="box-desc">Life Science / Physical Science / Mathematics / English</p>
           <h4 class="box-pricing"><sup>₹</sup>500<span> / month / Subject</span></h4>
           <p class="box-desc">Additional Tool Cost of ₹ 150 + GST</p>
         </div>
@@ -90,71 +152,6 @@
           <h3 class="box-heading">Entrance Examinations</h3>
           <!--<h4 class="box-pricing"><sup>₹</sup>400<span> / month</span></h4> -->
           <p class="box-desc">Kishore Vaigyanik Protsahan Yojana (KVPY) and Indian Institutes of Science Education and Research (IISER) <br /></p>
-          <p class="box-desc">Please send an e-mail to hr@odantapuri.net.in regarding your interest and we will get back to you.</p>
-        </div>
-      </div>
-    </div>
-
-    <div class="row tab-wrapper" id="quarterlyPlan">
-
-      <div class="col-lg-4 col-md-6">
-        <div class="box">
-          <h3 class="box-heading">Free</h3>
-          <h4 class="box-pricing"><sup>₹</sup>0<span> / month</span></h4>
-          <p class="box-desc">This is quarterly plan.</p>
-          <div class="btn-wrap">
-            <a href="#" class="btn-buy">Buy Now</a>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-lg-4 col-md-6 mt-4 mt-md-0">
-        <div class="box">
-          <h3 class="box-heading">Advanced</h3>
-          <h4 class="box-pricing"><sup>₹</sup>400<span> / month</span></h4>
-          <p class="box-desc">This is quarterly plan.</p>
-          <div class="btn-wrap">
-            <a href="#" class="btn-buy">Buy Now</a>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-lg-4 col-md-6 mt-4 mt-lg-0">
-        <div class="box">
-          <h3 class="box-heading">Expert</h3>
-          <h4 class="box-pricing"><sup>₹</sup>800<span> / month</span></h4>
-          <p class="box-desc">This is quarterly plan.</p>
-          <div class="btn-wrap">
-            <a href="#" class="btn-buy">Buy Now</a>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="row tab-wrapper" id="yearlyPlan">
-
-      <div class="col-lg-4 col-md-6">
-        <div class="box">
-          <h3 class="box-heading">Class 9 and Class 10</h3>
-          <p class="box-desc">Physics / Chemistry / Mathematics / English</p>
-          <h4 class="box-pricing"><sup>₹</sup>5000<span> / year / Subject + GST</span></h4>
-          <p class="box-desc">Student Collaboration tool will be provided free of cost.</p>
-        </div>
-      </div>
-
-      <div class="col-lg-4 col-md-6 mt-4 mt-md-0">
-        <div class="box">
-          <h3 class="box-heading">Class 11 and Class 12</h3>
-          <p class="box-desc">Physics / Chemistry / Mathematics / Biology / English</p>
-          <h4 class="box-pricing"><sup>₹</sup>10000<span> / year / Subject + GST</span></h4>
-          <p class="box-desc">Student Collaboration tool will be provided free of cost.</p>
-        </div>
-      </div>
-
-      <div class="col-lg-4 col-md-6 mt-4 mt-lg-0">
-        <div class="box">
-          <h3 class="box-heading">Entrance Examinations</h3>
-          <!--<h4 class="box-pricing"><sup>₹</sup>400<span> / month</span></h4> -->
-          <p class="box-desc">Kishore Vaigyanik Protsahan Yojana (KVPY) and Indian Institutes of Science Education and Research (IISER) <br /><br /> </p>
           <p class="box-desc">Please send an e-mail to hr@odantapuri.net.in regarding your interest and we will get back to you.</p>
         </div>
       </div>
